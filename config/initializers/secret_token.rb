0912be9043ec41929569d1d5750d6baae0e31347
Admin <adminrob@ubuntu.(none)>
2013-09-14 16:13:40 -0400

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirehouseApp::Application.config.secret_token = 'd32026cfc39e9d5d814743f98e3c3e5196e8bb03a22787452b6ee13e82a0b5b4f782a05aaabbf121c2e0b7e7bda7e39e90d0e7c63ccaa37abdc5d48c32b73f9e'
