27496f153bd7f204540ac49cb66bed86edc33ed9
Admin <adminrob@ubuntu.(none)>
2013-09-14 18:01:20 -0400

    Adding stuff to the new form

---
+++ app/controllers/pics_controller.rb
@@ -0,0 +1,26 @@
+class PicsController < ApplicationController
+	#new
+	def new
+		# Ruby code here
+		@pic = Pic.new
+	end
+
+	def index
+	end
+
+	def create 
+	end
+
+	def show
+	end
+
+	def destroy
+	end
+
+	def edit
+	end
+
+	def update
+	end
+
+end
